6ffe3c23e01b77b601ebd930155fd76c1a31dfb3
billmwong <billmwong@gmail.com>
2016-09-23 20:18:10 -0400

    deleted unused imports

--- lesson3/Lesson3HW/app/src/main/java/com/margaret/lesson3hw/MainActivityFragment.java
+++ lesson3/Lesson3HW/app/src/main/java/com/margaret/lesson3hw/MainActivityFragment.java
@@ -5,15 +5,11 @@ import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 /**
  * A placeholder fragment containing a simple view.
